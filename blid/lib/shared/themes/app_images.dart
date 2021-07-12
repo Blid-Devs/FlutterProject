ead357fdff7eefea7587345a3374b031ca524fa2
Lorena C Silva <loricristinesilva@gmail.com>
2021-07-12 11:53:07 -0300

    Correção

--- blid/lib/shared/themes/app_images.dart
+++ blid/lib/shared/themes/app_images.dart
@@ -1,13 +1,18 @@
 class AppImages {
-  static const BottomDecorationSquare = "assets/images/bottom-decoration-square" //Decoração quadrada de baixo
-  static const TopDecorationSquare = "assets/images/top-decoration-square" //Decoração quadrada de cima
-  static const BottomDecorationWave = "assets/images/top-decoration-wave" //Decoração Ondas de baixo
-  static const TopDecorationWave = "assets/images/top-decoration-wave" //Decoração Ondas de cima
-  static const Doctors = "assets/images/vetor-doutores.png" //Doutores
-  static const BloodBag = "assets/images/blood-bag.png" //bolsa de sangue
-  static const BloodTest = "assets/images/blood-test.png" //Teste de sangue
-  static const blood = "assets/images/blood.png" //Gotas de sangue
-  static const RedCells = "assets/images/erythrocytes.png"// Glóbulos vermelhos, Hemácias
-  static const Contacts = "assets/images/communicate.png" // Contatos
-  static const Writing = "assets/images/writing.png" //Folha de documentos
+  static const BottomDecorationSquare =
+      "assets/images/bottom-decoration-square"; //Decoração quadrada de baixo
+  static const TopDecorationSquare =
+      "assets/images/top-decoration-square"; //Decoração quadrada de cima
+  static const BottomDecorationWave =
+      "assets/images/top-decoration-wave"; //Decoração Ondas de baixo
+  static const TopDecorationWave =
+      "assets/images/top-decoration-wave"; //Decoração Ondas de cima
+  static const Doctors = "assets/images/vetor-doutores.png"; //Doutores
+  static const BloodBag = "assets/images/blood-bag.png"; //bolsa de sangue
+  static const BloodTest = "assets/images/blood-test.png"; //Teste de sangue
+  static const blood = "assets/images/blood.png"; //Gotas de sangue
+  static const RedCells =
+      "assets/images/erythrocytes.png"; // Glóbulos vermelhos, Hemácias
+  static const Contacts = "assets/images/communicate.png"; // Contatos
+  static const Writing = "assets/images/writing.png"; //Folha de documentos
 }
