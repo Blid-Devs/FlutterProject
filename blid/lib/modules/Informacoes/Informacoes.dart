fc0113aa3f8b227a195be59f755cc462f75f71f6
Lorena C Silva <loricristinesilva@gmail.com>
2021-08-04 23:01:29 -0300

    appbar

--- blid/lib/modules/Informacoes/Informacoes.dart
+++ blid/lib/modules/Informacoes/Informacoes.dart
@@ -1,3 +1,11 @@
 import 'package:blid/shared/themes/app_colors.dart';
 import 'package:blid/shared/themes/app_images.dart';
 import 'package:flutter/material.dart';
+      appBar: AppBar(
+        title: Text('I N F O R M A Ç Õ E S',
+            style: TextStyle(color: AppColors.azulEscuro)),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: AppColors.widgets,
+        toolbarHeight: 71,
+      ),
